d17fbcca4414f00703cd209c2e5fc7765f686533
Sabhatina Palani Sellvam <39486153+sabhatina@users.noreply.github.com>
2019-03-06 23:53:53 -0600

    Update obj_file_construction.py

--- obj_file_construction.py
+++ obj_file_construction.py
@@ -1,61 +1,69 @@
-
-from skimage import measure
-import scipy.io
-
 import numpy as np
-import math
-import scipy
+from numpy import linalg as LA
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
-import h5py
-def sphere(shape, radius, position):
+N=[0.3,0.5,0] #unit vector of the plane
+D=0.5
+rho=D/LA.norm(N,2)
+if(rho>1):
+    exit(0)
+center=rho*(N/LA.norm(N,2))
 
-    semisizes = (radius,) * 3
+radius=np.sqrt(1-rho**2)
+print(center)
+print(rho)
 
-
-    grid = [slice(-x0, dim - x0) for x0, dim in zip(position, shape)]
-    position = np.ogrid[grid]
-
-    arr = np.zeros(shape, dtype=float)
-    for x_i, semisize in zip(position, semisizes):
-        arr += (np.abs(x_i / semisize) ** 2)
-
-    return arr <= 1.0
-radius=10
-arr = sphere((10, 10, 10), radius, (0, 0, 0))
-
-
-
-angle=450
-
-c=np.cos(angle)
-s=-np.sin(angle)
-rx=np.asarray([[1,0,0],[0,c,-s],[0,s,c]])
-ry=np.asarray([[c,0,s],[0,1,0],[-s,0,c]])
-rz=np.asarray([[c,-s,0],[s,c,0],[0,0,1]])
-
-verts, faces, normals, values = measure.marching_cubes_lewiner(arr,None,(1,) * 3)
-
-verts=np.dot(verts,ry)
-
-verts[:,0]=-verts[:,0]
-verts[:,2]=verts[:,2]+8
-verts=verts*0.05
-verts=verts+0.25
-
-a=[]
-
-faces=faces+1
-
+verts=np.loadtxt('verts2.txt')
+faces=np.loadtxt('faces2.txt')
+normals=np.loadtxt('norms2.txt')
 normals=-normals
+c=0
+x=[]
+y=[]
+z=[]
+vert_index=[]
+
+new_verts=[]
+vert_delete=[]
+for i in range(len(verts)):
+
+    if (verts[i,0]>center[0]):# and verts[i,1]>center[1]):# and verts[i,2]>center[2]):#and verts[i,1]>center[1]
+        new_verts.append(verts[i])
+        x.append(verts[i,0])
+        y.append(verts[i,1])
+        z.append(verts[i, 2])
+        vert_index.append(i)
+
+    else:
+        vert_delete.append(i)
+
+row_to_delete=[]
+print(len(new_verts))
+print(len(faces))
+for i in range(len(faces)):
+
+    for j in vert_delete:
+        if (faces[i][0] == j or faces[i][1] == j or faces[i][2] == j):
+
+            row_to_delete.append(i)
+
+
+mylist = list(dict.fromkeys(row_to_delete))
+print(len(mylist))
+new_faces = np.delete(faces, mylist, axis=0)
+new_normals=np.delete(normals, mylist, axis=0)
+print(len(new_faces))
+
 
 thefile = open('test.obj', 'w')
 for item in verts:
   thefile.write("v {0} {1} {2}\n".format(item[0],item[1],item[2]))
 
-for item in normals:
+for item in new_normals:
   thefile.write("vn {0} {1} {2}\n".format(item[0],item[1],item[2]))
 
-for item in faces:
+for item in new_faces:
   thefile.write("f {0}//{0} {1}//{1} {2}//{2}\n".format(item[0],item[1],item[2]))
 thefile.write("\n")
 with open('origin.txt', 'r') as File:
@@ -64,7 +72,7 @@ with open('origin.txt', 'r') as File:
           thefile.write(line)
 
 thefile.write('\n')
-thefile.write("f {0}//{3} {1}//{3} {2}//{3} \n".format(len(verts)+1,len(verts)+2,len(verts)+3,len(normals)+1))
-thefile.write("f {0}//{3} {1}//{3} {2}//{3} \n".format(len(verts)+1,len(verts)+4,len(verts)+2,len(normals)+1))
+thefile.write("f {0}//{3} {1}//{3} {2}//{3} \n".format(len(verts)+1,len(verts)+2,len(verts)+3,len(new_normals)+1))
+thefile.write("f {0}//{3} {1}//{3} {2}//{3} \n".format(len(verts)+1,len(verts)+4,len(verts)+2,len(new_normals)+1))
 thefile.close()
 File.close()
